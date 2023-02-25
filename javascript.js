faffb28a03c7978360746c4e381a28b5273e455c
ejhall04 <halle14@nku.edu>
2023-02-24 20:20:12 -0500

    Lab 7

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function eventHandler() {
+   var x = document.getElementById("num1");
+   var y = document.getElementById("num2");
+   var numx = parseInt(x);
+   var numy = parseInt(y)
+   var max = Math.max(numx, numy);
+   setTimeout(() => { alert(numx) }, 3000);
+   setTimeout(() => { console.log(max) }, 3000);
+}
+
+let xhr = new XMLHttpRequest();
+let url = new URL('https://api.weather.gov/glossary')
+
+xhr.open('GET', url);
+
+xhr.send();
+xhr.responseType = 'json'
+xhr.onload = function() {
+    let responseObj = xhr.response;
+    setTimeout(() => { console.log(responseObj); }, 3000);
+};
